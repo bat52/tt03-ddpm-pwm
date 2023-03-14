9a7ca62034b7f437e3d040daaea54b5256958b5d
Marco Merlin <marcomerli@gmail.com>
2023-03-14 22:31:31 +0100

    started sd1_mod vcd postprocess

--- src/myhdl/sd1_mod.py
+++ src/myhdl/sd1_mod.py
@@ -3,6 +3,9 @@
 import sys
 import os
 import argparse
+from ast import literal_eval
+import matplotlib.pyplot as plt
+import numpy as np
 
 # NB: this requires installing myHDL from github, as follows
 # pip3 install git+https://github.com/myhdl/myhdl.git@master
@@ -11,12 +14,14 @@ from myhdl import *
 # pip3 install git+https://github.com/bat52/pueda.git@master
 from pueda.myhdl import *
 from pueda.common import get_clean_work, vcd_view
-from pueda.gtkw import gen_gtkw
-from pueda.yosys import yosys
+
+# https://pypi.org/project/vcdvcd/
+from vcdvcd import VCDVCD
 
 from sine_lut import sine_lut, NBIT_SINE_LUT_PHASE, NBIT_SINE_LUT_AMPLITUDE
 
 NBIT_SD = NBIT_SINE_LUT_AMPLITUDE + 2
+NBIT_SD_PRESCALER = NBIT_SINE_LUT_PHASE
 SD1_TOP = 'sd1_mod'
 SD1_TB_TOP = 'tb_' + SD1_TOP
 
@@ -81,21 +86,27 @@ def sd1_mod(clk, resetn, nbits = NBIT_SD, inval = Signal(intbv(0)[NBIT_SD:]), sd
 def tb_sd1_mod(work = './',
            nbit_sd = NBIT_SD,
            nbit_in = NBIT_SINE_LUT_PHASE, nbit_out = NBIT_SINE_LUT_AMPLITUDE,
+           nbit_prescaler = NBIT_SD_PRESCALER,
            dump_en = False, cosim_en = False, convert_en = False,
            period = 2, duration = 2**NBIT_SINE_LUT_PHASE):    
 
     clk       = Signal(bool(0))
     resetn    = ResetSignal(bool(0), active=False, isasync=True)
-    
+
+    count_out = Signal(intbv(0)[nbit_in+nbit_prescaler:])
     lut_in  = Signal(intbv(0)[nbit_in:])
     lut_out = Signal(intbv(0)[nbit_out:])
 
     sd_out = Signal(bool(0))
 
     sd1_i   = sd1_mod(clk, resetn, nbits = nbit_sd, inval = lut_out, sd_out = sd_out)
-    count_i = counter_up(clk, resetn, count_out = lut_in, nbits = nbit_in)
+    count_i = counter_up(clk, resetn, count_out = count_out, nbits = nbit_in+nbit_prescaler)
     lut_i   = sine_lut( nbits_amplitude = nbit_out, nbits_phase = nbit_in,
                         in_index = lut_in, sine_out = lut_out)
+    
+    @always_comb
+    def lut_in_proc():
+        lut_in.next = count_out[nbit_in+nbit_prescaler:nbit_prescaler]
     
     @instance
     def genclk():
@@ -136,7 +147,8 @@ def test_sd1_main( convert_en = False, dump_en = True,
     work = get_clean_work('sd1', makedir=True)
     os.system('rm *.vcd')
 
-    duration = 2**(NBIT_SINE_LUT_PHASE+1)-1
+    nperiods = 5
+    duration = nperiods * 2**(NBIT_SINE_LUT_PHASE+NBIT_SD_PRESCALER+1)-1
 
     tb_i = tb_sd1_mod(
              convert_en = convert_en, dump_en = dump_en, cosim_en = cosim_en,
@@ -146,7 +158,9 @@ def test_sd1_main( convert_en = False, dump_en = True,
         tb_i.config_sim(trace=dump_en)
         tb_i.run_sim(duration=duration)
         if dump_en:
-            sim_view()
+            vcd  = SD1_TB_TOP + '.vcd'
+            sim_view(vcd)
+            # sim_postproc(vcd)
     else: # cosim_en
         cosim = Simulation( tb_i )
         cosim.run(duration)
@@ -154,11 +168,92 @@ def test_sd1_main( convert_en = False, dump_en = True,
             cosim_view()
     pass
 
-def sim_view(nbits = 4):    
-    vcd  = SD1_TB_TOP + '.vcd'
-    gtkw = SD1_TB_TOP +'.gtkw'
+def sim_view(vcd):    
+    fname = os.path.splitext(vcd)[0]
+    gtkw = fname + '.gtkw'
     # gen_gtkw_ddpm(gtkw, nbits=nbits)
-    vcd_view(vcd, savefname=gtkw)
+    vcd_view(vcd, savefname=gtkw, block_en=False)
+    return vcd
+
+def vcd_tv2list(tv,fmt='dec'):
+    time = []
+    value = []
+    for e in tv:
+
+        if   fmt == 'dec':
+            vstr = e[1]
+        elif fmt == 'bin':
+            vstr = '0b' + str.strip(e[1])
+        elif fmt == 'hex':    
+            vstr = '0h' + str.strip(e[1])
+        else:
+            print('unsupported format "%s" !!!' % fmt)
+            assert(False)
+
+        value.append(literal_eval(vstr))
+        time.append(e[0])
+    return {'value':value,'time':time}
+
+def vcd_resample(s, fs=1):
+
+    # generate time series
+    tmin = min(s['time'])
+    tmax = max(s['time'])
+    t = np.arange(tmin,tmax,fs)
+    sout = np.interp(t, s['time'], s['value'])
+    # sout = f(t)
+
+    return {'value':sout, 'time': t}    
+
+def vcd_get_signal(vcdh,sname,fmt = 'dec', resample_en = False, fs=1e9):
+    signal = vcdh[sname]
+    # tv is a list of Time/Value delta pairs for this signal.
+    s = vcd_tv2list(signal.tv,fmt = fmt)
+    if resample_en:
+        s = vcd_resample(s)
+    return s 
+
+def sim_postproc(fname):
+    vcd = VCDVCD(fname)
+
+    # List all human readable signal names.
+    # print(vcd.references_to_ids.keys())
+
+    # View all signal data.
+    # print(vcd.data)
+
+    # Get a signal by human readable name.
+    sd_out  = vcd_get_signal( vcd, SD1_TB_TOP + '.sd_out' ,            resample_en=True)
+    lut_out = vcd_get_signal( vcd, SD1_TB_TOP + '.lut_out', fmt='bin', resample_en=True)
+
+    if True:
+        plt.figure(1)
+        plt.step(sd_out['time'],sd_out['value'])
+        plt.xlabel('time [ns]')
+        plt.ylabel('sd_out')
+        plt.show()
+
+    if True:
+        plt.figure(2)
+        plt.step(lut_out['time'],lut_out['value'])
+        plt.xlabel('time [ns]')
+        plt.ylabel('lut_out [LSB]')
+        plt.show()
+
+    sdf = np.array(lut_out['value']) 
+    sdf = np.fft.fft(sdf)
+    sdf = np.absolute(sdf)
+    sdf = sdf / np.max(sdf)
+    sdf = 20*np.log10(sdf)
+
+    plt.figure(3)
+    plt.semilogx(sdf)
+    plt.grid(True)
+    plt.xlabel('freq')
+    plt.ylabel('sd_out [dBc]')
+    plt.show()
+
+    pass
 
 def cosim_view():    
     vcd  = os.path.join( '../work_icarus', SD1_TOP + '.vcd')
