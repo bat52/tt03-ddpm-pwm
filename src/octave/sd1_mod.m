c29550f93d22d3e5db4e507221d974fbb03e0388
Marco Merlin <marcomerli@gmail.com>
2023-03-17 21:43:46 +0100

    sd1_mod.m supports multibit out

--- src/octave/sd1_mod.m
+++ src/octave/sd1_mod.m
@@ -1,20 +1,24 @@
 
-function sd_out = sd1_mod( inval,  nbits)
+function sd_out = sd1_mod( inval,  nbits, nbits_out)
+  
+  if nargin < 3
+    nbits_out = 1;
+  end
+  
+  assert(nbits > nbits_out);
 
-    sd_out = zeros(size(inval));
-    delta  = zeros(size(inval));
-    qerr   = zeros(size(inval));
+  sd_out = zeros(size(inval));
+  delta  = zeros(size(inval));
+  qerr   = zeros(size(inval));
 
-    maxval = 2^nbits -1;
-    minval = 0;
-      
-    for idx = 2:length(inval)
-        delta(idx) = saturated_adder( inval(idx),  qerr(idx-1),   maxval, minval);
-        qerr(idx)  = bitand( delta(idx), 2^(nbits-1) - 1);
-        if delta(idx) > 2**(nbits-1)
-          sd_out(idx) = 1;
-        end
-    end
+  maxval = 2^nbits -1;
+  minval = 0;
+    
+  for idx = 2:length(inval)
+      delta(idx)  = saturated_adder( inval(idx),  qerr(idx-1),   maxval, minval);
+      qerr(idx)   = bitand( delta(idx), 2^(nbits-nbits_out) - 1);
+      sd_out(idx) = bitshift(delta(idx),-(nbits-nbits_out));
+  end
     
   if 1
     fh = figure(541);
